03eb7d03c02eb2a70515e9965ead0bea8c5867ef
unknown <nasirlawal001@gmail.com>
2022-03-07 08:27:49 +0100

    add notification to the app

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/services/notification_services.dart';
 import 'package:flutter_todo_app/services/theme_services.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({ Key? key }) : super(key: key);
@@ -10,6 +14,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  var notifyHelper;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    notifyHelper = NotifyHelper();
+    notifyHelper.initializeNotification();
+    notifyHelper.requestIOSPermissions();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +46,12 @@ class _HomePageState extends State<HomePage> {
       leading: GestureDetector(
         onTap: () {
           ThemeServices().switchTheme();
+          notifyHelper.displayNotification(
+            title: "Theme Changed",
+            body: Get.isDarkMode ? "Activated Light Theme" : "Activated Dark Theme"
+          );
+
+          notifyHelper.scheduledNotification();
         },
         child: Icon(Icons.nightlight_round,
         size: 20,),
